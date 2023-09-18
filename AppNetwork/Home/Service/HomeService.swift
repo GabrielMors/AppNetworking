15dd37b4a39d4e08e7dc41c4038e7479ff48e5ff
Gabriel Mors <gabrielmors210@gmail.com>
2023-09-17 22:17:17 -0400

    criado file para networkError

--- AppNetwork/Home/Service/HomeService.swift
+++ AppNetwork/Home/Service/HomeService.swift
@@ -7,30 +7,6 @@
 
 import UIKit
 
-enum NetworkError: Error {
-    case invalidURL(url: String)
-    case noData
-    case invalidResponse
-    case decodingError (Error)
-    case networkFailure (Error)
-}
-extension NetworkError: LocalizedError {
-    var errorDescription: String? {
-        switch self {
-        case .invalidURL (let ur1):
-            return "URL inválida -> \(ur1)"
-        case . noData:
-            return "Dados não recebidos da API."
-        case .invalidResponse:
-            return "Resposta inválida da API."
-        case .decodingError(let error):
-            return "Decodificação falhou: \(error.localizedDescription)"
-        case .networkFailure(let error):
-            return "Falha na rede: \(error.localizedDescription)"
-        }
-    }
-}
-
 class HomeService: NSObject {
     
 //    func getPersonList(completion: @escaping (Result<PersonList,NetworkError>) -> Void) {
