c215f0c34c0d30ed9a8d35b44929920636357c1f
Gabriel Mors <gabrielmors210@gmail.com>
2023-09-18 11:15:03 -0400

    configurado serviceManager, e feito alteracoes

--- AppNetwork/Service/ServiceManager.swift
+++ AppNetwork/Service/ServiceManager.swift
@@ -9,70 +9,66 @@ import Foundation
 
 class ServiceManager: NetworkLayer {
     
-    // Singleton
     static var shared: ServiceManager = ServiceManager()
     
     private var baseURL: String
+    private var requestBuilder: RequestBuilder
+    private var session: URLSession
+    private var decoder: JSONDecoder
     
-    init(baseURL: String? = nil) {
+    init(session: URLSession = URLSession.shared, baseURL: String? = nil, requestBuilder: RequestBuilder = DefaultRequestBuilder(), decoder: JSONDecoder = JSONDecoder()) {
+        self.requestBuilder = requestBuilder
+        self.session = session
+        self.decoder = decoder
         
         if let baseURL {
             self.baseURL = baseURL
-                        // irá procurar no meu info.plist uma chave "BaseURL" e me retorna o valor
         } else if let baseURLString = Bundle.main.infoDictionary?["BaseURL"] as? String {
-        // caso ele encontrar o valor, UTILIZE ela
             self.baseURL = baseURLString
         } else {
             self.baseURL = ""
         }
     }
     
-    var session: URLSession = URLSession.shared
-    
-    func request<T>(with endPoint: Endpoint, decodeType: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) where T : Decodable {
-        
-        let urlString = baseURL + endPoint.url
-        
+    func request<T>(with endpoint: Endpoint, decodeType: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) where T : Decodable {
+
+        let urlString = baseURL + endpoint.url
         guard let url: URL = URL(string: urlString) else {
+            NetworkLogger.logError(error: .invalidURL(url: urlString))
             completion(.failure(.invalidURL(url: urlString)))
             return
         }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = endPoint.method.rawValue
-        
+
+        let request = requestBuilder.buildRequest(with: endpoint, url: url)
+
         let task = session.dataTask(with: request) { data, response, error in
+            NetworkLogger.log(request: request, response: response, data: data, error: error)
             DispatchQueue.main.async {
-                
                 if let error {
-                    print("ERROR \(#function) Detalhe do erro: \(error.localizedDescription)")
                     completion(.failure(.networkFailure(error)))
                     return
                 }
-                
+
                 guard let data else {
                     completion(.failure(.noData))
                     return
                 }
-                
+
                 guard let response = response as? HTTPURLResponse, 200...299 ~= response.statusCode else {
                     completion(.failure(.invalidResponse))
                     return
                 }
-                
+
                 do {
-                    let decoder = JSONDecoder()
-                    decoder.dateDecodingStrategy = .iso8601
-                    let object: T = try decoder.decode(T.self, from: data)
-                    print("SUCCESS -> \(#function)")
+                    let object: T = try self.decoder.decode(T.self, from: data)
                     completion(.success(object))
                 } catch  {
-                    print("ERROR -> \(#function)")
                     completion(.failure(.decodingError(error)))
                 }
             }
         }
         task.resume()
     }
+    
 }
 
